d9e3f6f71bfcfc62923ba19c80ae9fd25719c960
John Lira <johnalsoares@gmail.com>
2025-12-31 12:01:05 -0300

    feat (backend): Refactor checkout session in separate function

---
+++ backend/src/functions/post-checkout.ts
@@ -0,0 +1,30 @@
+import Stripe from "stripe";
+import { createCheckoutInput } from "../types/checkout.schema";
+import { env } from "../env";
+
+const stripe = new Stripe(env.STRIPE_SECRET_KEY);
+
+export const createCheckoutSession = async (body: createCheckoutInput) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: body.productName,
+            images: body.imageUrl ? [body.imageUrl as string] : undefined,
+          },
+          unit_amount: body.productPrice,
+        },
+        quantity: 1,
+      },
+    ],
+    customer_email: body.customerEmail,
+    mode: "payment",
+    success_url: "http://localhost:5173/success",
+  });
+  if (!session.url) {
+    throw new Error("Failed to create checkout session URL");
+  }
+  return session.url;
+};
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,18 +1,14 @@
 import { Elysia, redirect } from "elysia";
 import { health } from "./functions/health";
-import { z, ZodError } from "zod";
-import { Stripe } from "stripe";
-import { env } from "./env";
-import {
-  createCheckoutInput,
-  createCheckoutInputSchema,
-} from "./types/checkout.schema";
+import { z } from "zod";
+import { createCheckoutInputSchema } from "./types/checkout.schema";
+import { createCheckoutSession } from "./functions/post-checkout";
 
 export const r = new Elysia().get("/", () => "Hello Elysia");
 
-const stripe = new Stripe(env.STRIPE_SECRET_KEY);
-
+// Error handler
 r.onError(({ code, status, error }) => {
+  // Custom error zod
   if (code === "VALIDATION") return { error: error.customError };
 });
 
@@ -20,34 +16,10 @@ r.get("/health", health, {
   response: { 200: z.object({ status: z.string() }) },
 });
 
+// Create Checkout Session
 r.post(
   "/create-checkout-session",
-  async (ctx) => {
-    const body = ctx.body as createCheckoutInput;
-    const session = await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          price_data: {
-            currency: "usd",
-            product_data: {
-              name: body.productName,
-              images: body.imageUrl ? [body.imageUrl as string] : undefined,
-            },
-            unit_amount: body.productPrice,
-          },
-          quantity: 1,
-        },
-      ],
-      customer_email: body.customerEmail,
-      mode: "payment",
-      success_url: "http://localhost:3000/success",
-    });
-    if (!session.url) {
-      throw new Error("Failed to create checkout session URL");
-    }
-    console.log(session.url);
-    return session.url;
-  },
+  async ({ body }) => createCheckoutSession(body),
   {
     body: createCheckoutInputSchema,
     response: z.url(),
